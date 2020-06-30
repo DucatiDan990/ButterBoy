6c384a800d3464621f6214c12d3466f3a9df614c
Dan <Ducatidan@outlook.com>
2020-06-30 15:08:57 +1200

    Added Player Input Script Back

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayerInput : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public KeyCode Jump;
+    private Rigidbody2D RB;
+
+
+    public bool IsGrounded;
+    public float MaxJump;
+    private float CurrentJumpTime;
+
+    private void Awake()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+        }
+    }
     void Update()
     {
-        
+        if (Input.GetKey(Jump) && MaxJump > CurrentJumpTime)
+        {
+            RB.velocity = new Vector2(0, 10);
+            IsGrounded = false;
+
+            CurrentJumpTime += Time.deltaTime;
+        }
+
+        if (IsGrounded == true)
+        {
+            CurrentJumpTime = 0;
+        }
     }
 }
