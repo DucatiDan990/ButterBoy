48d269cb057229368c3bfc7fe23bf4e1d02c014d
Theodor <theodorenglund@gmail.com>
2020-07-02 15:34:04 +1200

    Working Movement animation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,6 +9,7 @@ public class PlayerInput : MonoBehaviour
     private Rigidbody2D RB;
 
     public bool IsGrounded;
+    bool jump = false;
     public float MaxJump;
 
     public float Speed;
@@ -29,16 +30,30 @@ public class PlayerInput : MonoBehaviour
             IsGrounded = true;
         }
     }
+
+
+
+
+
     void Update()
 
         // Jumping !!
     {
+        var animator = GetComponentInChildren<Animator>();
+
         if (Input.GetKey(Jump) && MaxJump > CurrentJumpTime)
         {
             RB.velocity = new Vector2(RB.velocity.x, 10);
             IsGrounded = false;
 
             CurrentJumpTime += Time.deltaTime;
+
+            jump = true;
+
+            if (animator != null)
+            {
+                animator.SetTrigger("jump");
+            }
         }
 
         if (IsGrounded == true)
@@ -51,6 +66,26 @@ public class PlayerInput : MonoBehaviour
         var horizontal = (Input.GetAxisRaw("Horizontal") * Speed);
 
         RB.velocity = new Vector2(horizontal, RB.velocity.y);
+
+        if (IsGrounded)
+        {
+
+            if (horizontal > 0)
+            {
+                animator.SetTrigger("right");
+            }
+
+            if (horizontal < 0)
+            {
+                animator.SetTrigger("left");
+            }
+
+            if (animator != null)
+            {
+                animator.SetTrigger("idle");
+            }
+        }
+
 
     }
 }
